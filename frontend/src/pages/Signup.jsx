9dff9384d0536cfa086adf93574a4aa1a52429ec
Prem <premkhodke.99@gmail.com>
2024-08-21 21:26:22 +0530

    signup page

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+const Signup = () => {
+
+  const [userDetails, setUserDetails] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const response = await axios.post('http://localhost:5000/api/user/register', userDetails);
+    const data = await response.data;
+    console.log(data);
+    if (data.success) {
+      navigate('/signin')
+    } else {
+      alert(data.message);
+    }
+  }
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setUserDetails({
+      ...userDetails,
+      [name]: value
+    });
+  }
+  
+  return (
+    <section>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-8 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Registration Form
+          </h1>
+        </div>
+
+        <div className="my-8 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                Full Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  autoComplete="name"
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-emerald-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-emerald-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-emerald-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                onClick={handleRegister}
+                className="flex w-full justify-center rounded-md bg-emerald-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-4 text-center text-sm text-gray-500">
+            Already a member?{' '}
+            <Link to={'/signin'} className="font-semibold leading-6 text-emerald-600 hover:text-emerald-500">
+              Login here
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Signup
