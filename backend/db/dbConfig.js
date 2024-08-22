97c6ff22d45772a2ab8961bc5ed61c54104e77f2
Prem <premkhodke.99@gmail.com>
2024-08-22 08:52:00 +0530

    mongo uri

--- backend/db/dbConfig.js
+++ backend/db/dbConfig.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const dbConnect = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(
+      "mongodb+srv://premktechworkspace23:Zfrz1O1B5ws6B6D9@cluster0.dbpsg.mongodb.net/"
+    )
     .then(() => console.log("DB Connected"))
     .catch((err) => console.error("DB Connection Failed", err));
 };
